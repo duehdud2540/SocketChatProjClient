97222a1afe3281207f2c9de8fe808b342e6344e8
아악 <duehdud1234@gmail.com>
2026-09-03 11:52:36 +0900

    first commit

---
+++ SocketChatCllient/ViewModel/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocketChatClient.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            // 앱 실행 시 첫 화면을 로그인 화면으로 세팅
+            CurrentViewModel = new LoginViewModel();
+        }
+    }
+}
